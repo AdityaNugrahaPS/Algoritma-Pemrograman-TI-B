589944eed61d5404b4c7eddc3f7bcb4b18569c98
mfasbir <mfsabran.jamil2@gmail.com>
2026-02-01 21:02:50 +0700

    Delete MFasbirSabranJamil_25071100361_TI-B/pertemuan-1 directory

--- MFasbirSabranJamil_25071100361_TI-B/pertemuan-1/tugas-pratikum/soal1.py
+++
@@ -1,5 +0,0 @@
-# Menyimpan bilangan ke dalam variabel
-nilai = 10
-
-# Menampilkan nilai variabel ke layar
-print("Nilai =", nilai)
--- MFasbirSabranJamil_25071100361_TI-B/pertemuan-1/tugas-pratikum/soal3.py
+++
@@ -1,8 +0,0 @@
-# Menyimpan nilai ujian (0-100) ke dalam variabel
-nilai_ujian = 75  # Bisa diganti dengan nilai lain
-
-# Mengecek kondisi kelulusan
-if nilai_ujian >= 60:
-    print("Lulus")
-else:
-    print("Tidak Lulus")
