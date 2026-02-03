007b07ae9ed75e1a9153517fd5bc825f9b790ad4
gledisnaomi1301 <gledis.naomi1301@student.unri.ac.id>
2026-02-03 12:08:57 +0700

    update tugas

---
+++ Gledis Naomi Marsella_25071101301_TI-B/Latihan Praktikum 1/soal2.py
@@ -0,0 +1,11 @@
+# Menyimpan dua bilangan ke dalam dua variabel
+a = 44
+b = 88
+
+# Menghitung penjumlahan dan perkalian
+penjumlahan = a + b
+perkalian = a * b
+
+# Menampilkan hasil ke layar
+print("Hasil penjumlahan =", penjumlahan)
+print("Hasil perkalian =", perkalian)
