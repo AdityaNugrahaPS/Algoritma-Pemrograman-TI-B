22060ae4e4d95824ac8e8f1fba1ee3ef5ec7e808
raydaveas-alt <redeabcdefg@gmail.com>
2026-02-03 21:18:41 +0700

    Tugas praktikum 1

---
+++ Ray Dave Adonia_25071103787_TI-B/Tugas praktikum/soal 13.py
@@ -0,0 +1,8 @@
+# Soal 1.3
+nilai = 75
+print(f"Nilai: {nilai}")
+if nilai >= 60:
+    print("Lulus.")
+else:
+    print("Tidak lulus.")
+print()
---
+++ Ray Dave Adonia_25071103787_TI-B/Tugas praktikum/soal 14.py
@@ -0,0 +1,8 @@
+# Soal 1.4
+total = 0
+
+for i in range(1, 11):
+    total += i
+
+print("Jumlah:", total)
+print()
