dd172e283cd3adf96f5860d1c146e24304dcc548
raydaveas-alt <redeabcdefg@gmail.com>
2026-02-03 21:08:45 +0700

    Tugas praktikum 1

---
+++ Tugas praktikum/soal 13.py
@@ -0,0 +1,9 @@
+# Soal 1.3
+print("Soal 1.3")
+nilai = 75
+print(f"Nilai: {nilai}")
+if nilai >= 60:
+    print("Lulus.")
+else:
+    print("Tidak lulus.")
+print()
---
+++ Tugas praktikum/soal 14.py
@@ -0,0 +1,9 @@
+# Soal 1.4
+print("Soal 1.4")
+total = 0
+
+for i in range(1, 11):
+    total += i
+
+print("Jumlah:", total)
+print()
